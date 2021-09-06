f247fb4219476ae3dfcdb8ec90400b1a9603ac42
inoue1231 <theremiomoncy@icloud.com>
2021-09-06 11:16:21 +0900

    ユーザー管理機能

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         validates :nickname, presence: true
+         validates :myouji, presence: true
+         validates :name, presence: true
+         validates :myouji_kana, presence: true
+         validates :name_kana, presence: true
+         validates :birth_date, presence: true
+
+
+end
