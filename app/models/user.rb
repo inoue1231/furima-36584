036f8e72bec2c23558914021531546c70137c77e
inoue1231 <theremiomoncy@icloud.com>
2021-09-07 09:48:05 +0900

    テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname, presence: true
-         validates :myouji, presence: true
-         validates :name, presence: true
-         validates :myouji_kana, presence: true
-         validates :name_kana, presence: true
+         validates :encrypted_password,:password,:password_confirmation,format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/}
+         validates :myouji, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+         validates :name, presence: true,format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+         validates :myouji_kana, presence: true,format: {with: /\A[ァ-ヶー－]+\z/ }
+         validates :name_kana, presence: true,format: {with: /\A[ァ-ヶー－]+\z/}
          validates :birth_date, presence: true
-
+  
 
 end
