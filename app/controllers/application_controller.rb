f3d8ba1a371a006ee43d1bc7c936fb3fdf6e3e57
inoue1231 <theremiomoncy@icloud.com>
2021-09-10 12:19:12 +0900

    商品出品機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-<<<<<<< Updated upstream
 
-=======
+
   before_action :configure_permitted_parameters, if: :devise_controller?
->>>>>>> Stashed changes
+
 
   private
 
@@ -13,11 +12,9 @@ class ApplicationController < ActionController::Base
       username == 'aaaa' && password == '12345'
     end
   end
-<<<<<<< Updated upstream
-=======
+
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :myouji, :name, :myouji_kana, :name_kana, :birth_date])
   end
->>>>>>> Stashed changes
 end
